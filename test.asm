7246982a72f1036220448a8c6a752b767363bb42
NIA <nia.fti@mail.ru>
2009-05-24 01:28:43 +0700

    Added bitwise AND and OR operations

--- test.asm
+++ test.asm
@@ -1,11 +1,19 @@
-          NOP   
-BEGIN     LDA   READ_DATA 
-          STA   FFFF_DATA 
-          LDA   FFFF_DATA 
-          ADD   ADD_DATA 
-          SUB   ADD_DATA 
-          JMP   BEGIN 
-          HLT   
-ADD_DATA  CON   0x0101;  some data to add 
-FFFF_DATA CON   0xFFFF;  some data to read 
-READ_DATA CON   0xA0A0;  some data to read 
+          NOP
+BEGIN     LDA   READ_DATA
+          STA   FFFF_DATA
+          LDA   FFFF_DATA
+          ADD   ADD_DATA
+          SUB   ADD_DATA
+          AND   HALF
+          OR    STRIPS
+          JMP   EXIT
+          NOP
+          NOP
+          NOP
+EXIT      HLT   
+
+ADD_DATA  CON   0x0101  ; some data to add 
+FFFF_DATA CON   0xFFFF  ; some data to read 
+READ_DATA CON   0xA0A0  ; some data to read 
+HALF      CON   0x00FF  ; some data to do bitwise add with
+STRIPS    CON   0x0F0F  ; some data to do bitwise or with
