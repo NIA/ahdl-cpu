8dd191a62446d90a6773bc3a7fd476937ae31696
NIA <nia.fti@mail.ru>
2009-05-25 05:41:39 +0700

    Program: added ability to test more than one multiplication per run, and added three tests for multiplication without fraction overflow

--- floatmult.asm
+++ floatmult.asm
@@ -1,3 +1,22 @@
+TEST1       LDA   TEST1_A
+            STA   A
+            LDA   TEST1_B
+            STA   B
+            JMP   START
+TEST2       LDA   TEST2_A
+            STA   A
+            LDA   TEST2_B
+            STA   B
+            JMP   START
+TEST3       LDA   TEST3_A
+            STA   A
+            LDA   TEST3_B
+            STA   B
+            JMP   START
+
+START       LDA   TEST_NUM
+            ADD   ONE
+            STA   TEST_NUM
             ;-- parse A ---
             LDA   A
             AND   FRACT_MASK
@@ -21,6 +40,7 @@
             JE    IF_NULL
             STA   EXPONENT_B
             ;-- calculate EXPONENT ---
+            ; now we have exponent of B in rA
             LDA   EXPONENT_A
             ADD   EXPONENT_B
             SUB   EXPON_XCS
@@ -45,16 +65,26 @@
 IF_NULL     STA   RESULT
             JMP   FINISH
             ;------ END ---------
-FINISH      HLT
+FINISH      LDA   TEST_NUM
+            CMP   TWO
+            JL    TEST2
+            CMP   THREE
+            JL    TEST3
+
+            HLT
 
 ; ------- VAR & CONST ----------------
             ORIG  128
     ;- - INPUT DATA & RESULT - - - -
-A           CON   0x4000 ; (1+0)*2^1 = 2
-B           CON   0x0000 ; (1+0)*2^1 = 2
+A           CON   0x0000
+B           CON   0x0000
 RESULT      CON   0x0000
     ;- - - CONSTANTS - - - - - - - -
-NULL        CON   0x0000
+NULL        CON   0
+ONE         CON   1
+TWO         CON   2
+THREE       CON   3
+FOUR        CON   4
 FRACT_MASK  CON   0x07FF ; b"0000011111111111"
 EXPON_MASK  CON   0x7800 ; b"0111100000000000"
 
@@ -67,7 +97,16 @@ FRACTION_A  CON   0x0000
 FRACTION_B  CON   0x0000
 EXPONENT_A  CON   0X0000
 EXPONENT_B  CON   0x0000
-            
+
+TEST_NUM    CON   0
+
             ORIG  220
 FRACT_RES   CON   0x0000
 EXPON_RES   CON   0x0000
+    ;- - - TEST CASES - - - - - - - 
+TEST1_A     CON   0x4800 ; (1+0)*2^2 = 4
+TEST1_B     CON   0x5000 ; (1+0)*2^3 = 8
+TEST2_A     CON   0x4000
+TEST2_B     CON   0x0000 ; 0
+TEST3_A     CON   0x3A00 ; 1.25
+TEST3_B     CON   0x3900 ; 1.125
